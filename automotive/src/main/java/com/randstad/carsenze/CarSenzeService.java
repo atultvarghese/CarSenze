e9f053fe8e28d9ea5f7dbada3b953c7fd69ab695
jyothika-k <jyothika.kizhakkedath@randstaddigital.com>
2024-05-31 16:00:37 +0530

    create a service

--- automotive/src/main/java/com/randstad/carsenze/CarSenzeService.java
+++ automotive/src/main/java/com/randstad/carsenze/CarSenzeService.java
@@ -3,6 +3,7 @@ package com.randstad.carsenze;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
 
 public class CarSenzeService extends Service {
     public CarSenzeService() {
@@ -10,7 +11,7 @@ public class CarSenzeService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+            throw new UnsupportedOperationException("Not yet implemented");
     }
 }
