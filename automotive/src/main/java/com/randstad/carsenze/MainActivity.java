bdc18033689987ed8be62e9888608833f98ab531
atultvarghese <atultvarghese@gmail.com>
2024-05-31 17:44:05 +0530

    Main activity with cpp call services

--- automotive/src/main/java/com/randstad/carsenze/MainActivity.java
+++ automotive/src/main/java/com/randstad/carsenze/MainActivity.java
@@ -1,6 +1,13 @@
 package com.randstad.carsenze;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -8,6 +15,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IStatsService iStatsService;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            iStatsService = IStatsService.Stub.asInterface(service);
+
+            try {
+                TextView memory = findViewById(R.id.memory);
+                memory.setText(iStatsService.getMemoryStats());
+
+                TextView cpu = findViewById(R.id.cpu);
+                cpu.setText(iStatsService.getCpuStats());
+
+                TextView network = findViewById(R.id.network);
+                network.setText(iStatsService.getNetworkStats());
+            } catch (RemoteException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            iStatsService = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +55,12 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        bindService(new Intent(this, CarSenzeService.class), serviceConnection, Context.BIND_AUTO_CREATE);
+    }
 
-
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(serviceConnection);
     }
 }
